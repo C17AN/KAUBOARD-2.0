6a74ca60da0a978112295ca9d22560c6e2e1cbe3
chanmin, kim <kimchanmin1@gmail.com>
2022-06-02 22:02:11 +0900

    chore: initial app setting

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { MouseEvent } from "react";
+import ReactDOM from "react-dom";
+import { motion } from "framer-motion";
 
 type ModalProps = {
   children: React.ReactNode;
-  onClose: () => void;
 };
 
-const Modal = ({ children, onClose }: ModalProps) => {
+const ModalPortal = ({ children }) => {
+  const el = document.getElementById("modal-root");
+  if (el) {
+    return ReactDOM.createPortal(children, el);
+  } else {
+    return <>{children}</>;
+  }
+};
+
+const Modal = ({ children }: ModalProps) => {
+  const initialModalAnimation = {
+    y: -30,
+    scale: 0.7,
+  };
+
+  const activeModalAnimation = {
+    y: 0,
+    scale: 1,
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-600 bg-opacity-50" onClick={onClose}>
-      {children}
-    </div>
+    <ModalPortal>
+      <div className="fixed flex items-center justify-center top-0 left-0 w-full h-full bg-gray-600 bg-opacity-40">
+        <motion.div initial={initialModalAnimation} animate={activeModalAnimation}>
+          {children}
+        </motion.div>
+      </div>
+    </ModalPortal>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import Dashboard from "./pages/Dashboard";
 import DesktopLayout from "./layouts/Desktop";
 import Community from "./pages/Community";
-import Info from "./pages/Info";
+import Info from "./pages/Utility";
 import Gallery from "./pages/Gallery";
+import Settings from "./pages/Settings";
+import { initializeApp } from "firebase/app";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import "firebase/auth";
+import "firebase/database";
 import "./styles/index.css";
 import "./styles/reset.css";
 
@@ -13,13 +18,16 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <BrowserRouter>
-    <DesktopLayout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/info" element={<Info />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/community" element={<Community />} />
-      </Routes>
-    </DesktopLayout>
+    <AnimatePresence>
+      <DesktopLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/setting" element={<Settings />} />
+        </Routes>
+      </DesktopLayout>
+    </AnimatePresence>
   </BrowserRouter>
 );
