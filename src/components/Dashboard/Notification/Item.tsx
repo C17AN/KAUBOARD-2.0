a6f7a86b46adf7dff0c161f7d2a2a7084d7043a0
chanmin, kim <kimchanmin1@gmail.com>
2022-06-07 17:30:51 +0900

    feat: implement notification component

--- src/components/Dashboard/Notification/Item.tsx
+++ src/components/Dashboard/Notification/Item.tsx
@@ -6,13 +6,24 @@ type NotificationItemProps = {
 };
 
 const NotificationItem = ({ noticeType, title }: NotificationItemProps) => {
+  const handleNavigation = (path: string) => {
+    window.open(path, "_blank");
+  };
+
   return (
-    <li className="hover:bg-slate-100 hover:text-black hover:font-semibold text-slate-700 p-4 rounded-lg transition-all">
-      {noticeType === "GENERAL" ? (
-        <a href={"https://www.kau.ac.kr/web/pages/gc32172b.do"}>{title}</a>
-      ) : (
-        <a href={"https://www.kau.ac.kr/web/pages/gc14561b.do"}>{title}</a>
-      )}
+    <li
+      className="list-disc list-inside hover:bg-kau-primary/10 leading-5 hover:text-black text-slate-700/80 px-4 py-2 rounded-lg transition-all cursor-pointer hover:border-gray-200 hover:shadow overflow-hidden text-ellipsis whitespace-nowrap"
+      onClick={() => {
+        if (noticeType === "GENERAL") {
+          const path = "https://www.kau.ac.kr/web/pages/gc32172b.do";
+          handleNavigation(path);
+        } else {
+          const path = "https://www.kau.ac.kr/web/pages/gc14561b.do";
+          handleNavigation(path);
+        }
+      }}
+    >
+      {title}
     </li>
   );
 };
--- src/components/Dashboard/Notification/index.tsx
+++ src/components/Dashboard/Notification/index.tsx
@@ -1,62 +1,67 @@
+import { SpeakerphoneIcon } from "@heroicons/react/outline";
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import getNotification from "../../../apis/notification";
 import NotificationItem from "./Item";
 
 const Notice = () => {
+  // 쿼리 사용 이유 : 대시보드 -> 다른 페이지 -> 대시보드 처럼 페이지를 이동할 때, 매번 데이터를 새로 불러오지 않도록
+  const { data, dataUpdatedAt, isLoading } = useQuery("notification", getNotification, {
+    // onSuccess: ({ data }: { data: { generalNotiList: string[]; schoolNotiList: string[] } }) => {
+    // onSuccess는 실제 쿼리를 수행했을 때에만 실행되므로, 여기에 setState 로직을 사용하면 안된다.
+    // },
+    // cacheTime: 2 * 60 * 1000,
+    staleTime: 2 * 60 * 1000,
+  });
+
   useEffect(() => {
-    fetch("https://calm-mesa-43659.herokuapp.com/notification")
-      .then((res) => res.json())
-      .then((data) => {
-        setGeneralNotiList(data.generalNotiList.slice(0, 4));
-        setSchoolNotiList(data.schoolNotiList.slice(0, 4));
-        setIsLoading(false);
-      });
-  }, []);
+    setGeneralNotiList(data?.data?.generalNotiList ?? []);
+    setSchoolNotiList(data?.data?.schoolNotiList ?? []);
+  }, [isLoading]);
 
-  const [generalNotiList, setGeneralNotiList] = useState([]);
-  const [schoolNotiList, setSchoolNotiList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const isNewItem = (item) => {
-    if (
-      (new Date(item.date).getDate() === new Date().getDate() ||
-        new Date(item.date).getDate() === new Date().getDate() - 1) &&
-      new Date(item.date).getMonth() === new Date().getMonth()
-    ) {
-      return "new";
-    } else {
-      return "old";
-    }
-  };
+  const [generalNotiList, setGeneralNotiList] = useState<string[]>([]);
+  const [schoolNotiList, setSchoolNotiList] = useState<string[]>([]);
 
   return (
-    <div className="flex gap-8 bg-white py-4 px-6 rounded-md border-[1px] border-gray-200 border-solid shadow-sm">
-      <nav className="mb-2 flex-1">
-        <div className="mb-2">
-          <span className="mr-1 text-lg font-bold">일반공지</span>
-          <span className="text-sm text-gray-500">General Notice</span>
+    <>
+      <div className="flex justify-between items-end my-4">
+        <div className="flex space-x-2 items-center">
+          <SpeakerphoneIcon className="w-5 h-5 text-gray-600" />
+          <h2 className="text-xl font-bold">공지사항</h2>
         </div>
-        <ul className="flex flex-col gap-2">
-          {isLoading
-            ? "로딩 중..."
-            : generalNotiList.map((item, idx) => (
-                <NotificationItem key={idx} noticeType="GENERAL" title={item} />
-              ))}
-        </ul>
-      </nav>
-      <nav className="flex-1">
-        <div className="mb-2">
-          <span className="mr-1 text-lg font-bold">학사공지</span>
-          <span className="text-sm text-gray-500">Academic Notice</span>
+        <div className="text-xs text-gray-600 font-light">
+          최근 업데이트 : {new Date(dataUpdatedAt).toLocaleTimeString()}
         </div>
-        <ul className="flex flex-col gap-2">
-          {isLoading
-            ? "로딩 중..."
-            : schoolNotiList.map((item, idx) => (
-                <NotificationItem key={idx} noticeType="SCHOOL" title={item} />
-              ))}
-        </ul>
-      </nav>
-    </div>
+      </div>
+      <div className="flex mb-2 space-x-8 bg-white/80 backdrop-blur-sm py-4 px-6 rounded-md border-[1px] border-gray-200 border-solid shadow-sm">
+        <nav className="flex-1">
+          <div className="mb-2 pb-2 border-b-[1px] border-slate-200 border-solid">
+            <span className="mr-1 text-lg font-bold text-gray-700">일반공지</span>
+            <span className="text-sm text-gray-500">General Notice</span>
+          </div>
+          <ul className="flex flex-col gap-1">
+            {isLoading
+              ? "로딩 중..."
+              : generalNotiList.map((item, idx) => (
+                  <NotificationItem key={idx} noticeType="GENERAL" title={item} />
+                ))}
+          </ul>
+        </nav>
+        <nav className="flex-1">
+          <div className="mb-2 pb-2 border-b-[1px] border-slate-200 border-solid">
+            <span className="mr-1 text-lg font-bold text-gray-700">학사공지</span>
+            <span className="text-sm text-gray-500">Academic Notice</span>
+          </div>
+          <ul className="flex flex-col gap-1">
+            {isLoading
+              ? "로딩 중..."
+              : schoolNotiList.map((item, idx) => (
+                  <NotificationItem key={idx} noticeType="SCHOOL" title={item} />
+                ))}
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
 
