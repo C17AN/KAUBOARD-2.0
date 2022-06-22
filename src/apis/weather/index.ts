78397add85a26b38a6fa78f184f41c5c3000c217
chanmin, kim <kimchanmin1@gmail.com>
2022-06-22 22:01:52 +0900

    feat: implement weather fetching api

--- src/apis/weather/index.ts
+++ src/apis/weather/index.ts
@@ -2,13 +2,9 @@ import axios from "axios";
 import { BACKEND_ENDPOINT } from "../../utils/contants/endpoints";
 
 const getWeather = async () => {
-  try {
-    const path = `${BACKEND_ENDPOINT}/weather`;
-    const { data } = await axios.get(path);
-    return { status: 200, data };
-  } catch (err) {
-    return { status: 500, data: err };
-  }
+  const path = `${BACKEND_ENDPOINT}/weather`;
+  const { data } = await axios.get(path);
+  return data;
 };
 
 export default getWeather;
